e6357bb25c1305e0a280145550da4df8f6a43219
srirammanikumar <sriram.manikumar@gmail.com>
2017-07-22 18:20:11 -0400

    after test file

--- src/test/java/HelloWorld/HelloWorld/AppTest.java
+++ src/test/java/HelloWorld/HelloWorld/AppTest.java
@@ -4,17 +4,11 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+   
     public AppTest( String testName )
     {
         super( testName );
@@ -28,11 +22,10 @@ public class AppTest
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+   
     public void testApp()
     {
-        assertTrue( true );
+    	
+        assertEquals("abc", "Hello World");
     }
 }
